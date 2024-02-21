1c7267a3e9130fd1a29afc12ba8d42bd24e94933
HandharJunino <hjunino@gmail.com>
2024-02-21 12:25:17 +0000

    firebase try init

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@ import 'pages/sign_in_up.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
-  /*await Firebase.initializeApp(
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );*/
+  );
   runApp(const MyApp());
 }
 
