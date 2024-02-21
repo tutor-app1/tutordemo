e036c82e3f7309a1a89a34885f1d8e7b3ab870ea
HandharJunino <hjunino@gmail.com>
2024-02-21 11:31:26 +0000

    ignore firebase initialisation for now

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ import 'pages/sign_in_up.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-Future<void> main() async {
-  await Firebase.initializeApp(
+void main() {
+  /*await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
+  );*/
   runApp(const MyApp());
 }
 
